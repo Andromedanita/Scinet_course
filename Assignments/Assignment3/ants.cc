4db1f4de26d73de338e8f3ecf1fba856890dc184
“Anita <bahmanyar@astro.utoronto.ca>
2016-02-09 15:22:59 -0500

    Added timing for each time step and added the printing of the times

--- Assignments/Assignment3/ants.cc
+++ Assignments/Assignment3/ants.cc
@@ -10,6 +10,8 @@ compute the mean number of ants on each square of the table.
 #include <rarray>
 #include <ticktock.h>
 
+using namespace std;
+
 int main()
 {
     // ants walk on a table
@@ -19,6 +21,7 @@ int main()
     float     frac_move  = 0.2;  // fraction of ants moving
     const int total_ants = 1010; // initial number of ants
     TickTock stopwatch;
+    TickTock timer_single;
 
     // defining arrays of velocity, number and the new number of ants using rarray
     // so that we do not need to handle pointers to pointers 
@@ -32,10 +35,14 @@ int main()
     
     // run simulation
     for (int t = 0; t < end_time; t++) {
+        timer_single.tick();
         single_time_step(table_size, t, number_of_ants, new_number_of_ants, velocity_of_ants, frac_move);
+	timer_single.tock("Elapsed time for this step is:");
+	cout << "\n" << endl;
+	
     }
 
-    stopwatch.tock("Elapsed time is:");
+    stopwatch.tock("Total elapsed time is:");
     return 0;
     
 }             
