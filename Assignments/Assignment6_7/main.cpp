41273c1b0e1a1ce3e696acb87ed779f23a2aff28
Andromedanita <anita.bahmanyar@gmail.com>
2016-03-09 18:48:13 -0500

    cleared extra commented lines

--- Assignments/Assignment6_7/main.cpp
+++ Assignments/Assignment6_7/main.cpp
@@ -11,60 +11,6 @@
 
 using namespace std;
 
-/*
-// Power spectrum function
-rarray<double,1> power_spec(rarray<complex<double>,1> FT_signal){
-  rarray<double,1> power_spec(FT_signal.size());
-  for (int j=0;j<FT_signal.size();j++){
-    complex<double> prod = FT_signal[j] * conj(FT_signal[j]);
-    power_spec[j] = prod.real();
-  }
-  return power_spec;
-}
-
-// Correlation function
-double corr(rarray<double,1> F, rarray<double,1> G){
-  double FF = cblas_ddot(F.size(), F.data(), 1, F.data(), 1);
-  double GG = cblas_ddot(G.size(), G.data(), 1, G.data(), 1);
-  double FG = cblas_ddot(F.size(), F.data(), 1, G.data(), 1);
-  double corr_val = FG/sqrt(FF * GG);
-
-  return corr_val;
-}
-
-
-rarray<complex<double>,1> Fourier_Trans(rarray<complex<double>,1> signal){
-  // creating the fourier transform of an array                                                                                          
-  int n       = signal.size();
-  rarray<complex<double>,1> FT_signal(signal.size());
-  fftw_plan p = fftw_plan_dft_1d(n,(fftw_complex*)signal.data(), (fftw_complex*)FT_signal.data(),FFTW_FORWARD, FFTW_ESTIMATE);
-  fftw_execute(p);
-  fftw_destroy_plan(p);
-  return FT_signal;
-}
-
-
-rarray<complex<double>,1> read_data(string dir,string filename){
-  // openning the file
-  ifstream f(dir+filename);
-
-  // create empty arrays for time and signal                                                                                             
-  rarray<double,1> times;
-  rarray<complex<double>,1> signal;
-
-  // read in the signal                                                                                                                  
-  f >> times;
-  f >> signal;  
-
-  //rarray<complex<double>,2> columns;
-  //for (int t=0; t<times.size(); t++){
-  //  columns[t][0] = times[t];
-  //  columns[t][1] = signal[t];
-  //}
-
-  return signal;
-}
-*/
 
 int main(){
   rarray<complex<double>,1> GW_signal = read_data("/home/b/bovy/bahmanya/Assignments/hw6_7/gwdata/","GWprediction.rat");
@@ -90,7 +36,7 @@ int main(){
 
     double C = corr(F, G);
 
-    cout << "correlation is:" << C << endl;
+    cout << "correlation is:" << C << "\n" << endl;
     correlation_array[k] = C;
   }
   
