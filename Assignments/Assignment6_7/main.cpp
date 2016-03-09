2d1b7ae181f05d93cd68146f279c2fc0dd4b2357
Andromedanita <anita.bahmanyar@gmail.com>
2016-03-08 23:49:56 -0500

    writing main file

--- Assignments/Assignment6_7/main.cpp
+++ Assignments/Assignment6_7/main.cpp
@@ -23,51 +23,53 @@ void corr(rarray<double,1> F, rarray<double,1> G){
   double FF = cblas_ddot(F.size(), F.data(), 1, F.data(), 1);
   double GG = cblas_ddot(G.size(), G.data(), 1, G.data(), 1);
   double FG = cblas_ddot(F.size(), F.data(), 1, G.data(), 1);
-  
   double corr_val = FG/sqrt(FF * GG);
 
   return corr_val;
-  
 }
 
 
-int main(){
-  // open the file named f
-  ifstream f("/home/b/bovy/bahmanya/Assignments/hw6_7/gwdata/GWprediction.rat");
-  // create empty arrays for time and signal
-  rarray<double,1> times;
-  rarray<complex<double>,1> signal;
-  // read in the signal
-  f >> times;
-  f >> signal;
-
-  // creating the fourier transform of an array
+void Fourier_Trans(){
+  // creating the fourier transform of an array                                                                                          
   int n       = signal.size();
   rarray<complex<double>,1> FT_signal(n);
-
   fftw_plan p = fftw_plan_dft_1d(n,(fftw_complex*)signal.data(), (fftw_complex*)FT_signal.data(),FFTW_FORWARD, FFTW_ESTIMATE);
-
   fftw_execute(p);
   fftw_destroy_plan(p);
+  return FT_signal;
+}
+
+
+void read_data(string dir,string filename){
+  // openning the file
+  ifstream f(dir+filename);
+
+  // create empty arrays for time and signal                                                                                             
+  rarray<double,1> times;
+  rarray<complex<double>,1> signal;
+
+  // read in the signal                                                                                                                  
+  f >> times;
+  f >> signal;  
+
+  return signal;
+}
+
+int main(){
+  rarray<complex<double>,1> GW_signal = read_data("/home/b/bovy/bahmanya/Assignments/hw6_7/gwdata/","GWprediction.rat");
+  rarray<complex<double>,1> detection = read_data("/home/b/bovy/bahmanya/Assignments/hw6_7/gwdata/","detection01.rat");
   
-  cout << "signal FT is:" << FT_signal << "\n"  << endl;
+  rarray<double,1> F = power_spec(detection);
+  rarray<double,1> G = power_spec(GW_signla);
+  
+  double C = corr(F, G);
 
   
+  //for (int k=0; k<32;k++){
+    
 
-  // power spectrum for GWprediction.rat = F
-  //rarray<double,1> power_spec;
-  //for (int j=0;j<FT_signal.size(),j++){
-  //  rarray<complex<double>,1> prod = FT_signal[j] * conj(FT_signal[j]);
-  //  power_spec[j] = prod.real()
   //}
-
-  // compute G  which is the fourier transform of detection01.rat. . .detection32.rat files
   
-
-  // compute correlation between F and G
-
-
-  // repeat this for each of the detection01.rat ... detection32.rat files
-
+  
   return 0;
 }
