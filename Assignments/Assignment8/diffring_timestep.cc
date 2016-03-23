baea41cf2a8105a4e6b9a0ba69ba1dcf8ae368b3
Andromedanita <anita.bahmanyar@gmail.com>
2016-03-23 17:21:26 -0400

    filled in the blank inside each function and it works now

--- Assignments/Assignment8/diffring_timestep.cc
+++ Assignments/Assignment8/diffring_timestep.cc
@@ -13,6 +13,14 @@
 void perform_time_step(const rarray<double,2>& F, rarray<double,1>& P)
 {
     // PART OF THE ASSIGNMENT!
+  int n = F.extent(0);
+  rarray<double,1> PP(P.size());
+  // copying P into PP and the opposite
+  std::swap(P,PP);
+
+  float a = 1.0;
+  float b = 0.0; 
+  cblas_dgemv(CblasRowMajor, CblasNoTrans, n, n, a, F.data(), n, PP.data(), 1, b, P.data(), 1);
 }
 
 // fill the matrix needed in perform_time_step
@@ -22,5 +30,23 @@ void perform_time_step(const rarray<double,2>& F, rarray<double,1>& P)
 // dx: the spatial resolution
 void fill_time_step_matrix(rarray<double,2>& F, double D, double dt, double dx)
 {
-    // PART OF THE ASSIGNMENT!
-}
+  int n = F.extent(0);
+  
+  F.fill(0.0);
+  double off_diag = (D * dt)/(dx*dx); // off-diagonal values
+  double diag     = 1-2*off_diag;     // diagonal values
+
+  for (int i  = 1; i < n-1; i++){
+    F[i][i-1] = off_diag;  
+    F[i][i+1] = off_diag;
+    F[i][i]   = diag; 
+  }
+  // boundary conditions
+  F[0][n-1] = off_diag;
+  F[n-1][0] = off_diag;
+  F[n-1][n-1] = diag;
+  F[n-1][n-2] = off_diag;
+  F[n-2][n-1] = off_diag;
+  F[0][1] = off_diag;
+  F[0][0] = diag;
+  }
--- Assignments/Assignment8/walkring_timestep.cc
+++ Assignments/Assignment8/walkring_timestep.cc
@@ -7,6 +7,11 @@
 #include "walkring_timestep.h"
 #include <random>
 
+using namespace std;
+
+default_random_engine engine;
+uniform_real_distribution<double> uniform(0.0,1.0);
+
 // perform a single time step for the random walkers
 // pos: the positions of Z walkers. Z=pos.size()
 // N: the number of grid points. All positions should remain between 0
@@ -15,7 +20,39 @@
 //    the probability to stay on the same spot is thus 1-2p.
 void perform_time_step(rarray<int,1>& pos, int N, double p)
 {
-    // PART OF THE ASSIGNMENT!
+  // PART OF THE ASSIGNMENT!
+  //default_random_engine engine;
+  //uniform_int_distribution<int> uniform;
+  int Z = pos.size();
+  //int seed = 1.0;
+  //engine.seed(seed);
+
+  //rarray<double,1>walker;
+  //walker.fill(0.0);
+  for (int i=0;i<Z;i++){
+    double state = uniform(engine);
+    if (state>0 and state<p){
+      //move to the left
+      pos[i] -=1;
+      // boundary periodicity
+      if (pos[i]<0){
+	pos[i] += N;
+      }
+    }
+    else if (state>=p and state<(2*p)){
+      // move to the right
+      pos[i] += 1;
+      // boundary periodicity
+      if (pos[i]>N){
+	pos[i] -= N;
+      }
+    }
+    else if (state>=(2*p) and state<1){
+      //stay at the same position
+      pos[i] = pos[i];
+    }
+  }
+
 }
 
 
